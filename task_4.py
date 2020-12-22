5940ad3b2aff7f10c080621fa813fa94096df419
Andrei Brazhelovich <baatol.mr@gmail.com>
2020-12-22 23:56:30 +0300

    Fourth task of homework

---
+++ task_4.py
@@ -0,0 +1,16 @@
+"""Даны два действительных числа. Найти среднее
+арифметическое и среднее геометрическое этих
+чисел."""
+
+
+x = 6
+y = 7
+
+solution_1  = (x + y) / 2
+solution_2 = pow(x * y, (1 / 2))
+
+print("X: " + str(x))
+print("Y: " + str(y) + "\n")
+
+print("Solution_1: " + str(solution_1))
+print("Solution_2: " + str(solution_2))
