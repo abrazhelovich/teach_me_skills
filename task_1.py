d70888523f87fc54d9dee0cd022079d4bcc1d227
Andrei Brazhelovich <baatol.mr@gmail.com>
2020-12-22 22:34:48 +0300

    First task of homework

---
+++ task_1.py
@@ -0,0 +1,14 @@
+#  Даны 2 действительных числа a и b. Получить их сумму, разность и произведение
+
+a = 7
+b = 9
+
+summa = a + b
+difference = b - a
+multipl = a * b
+
+print("a = 7")
+print("b = 9")
+print("\nSumm: " + str(summa))
+print("Difference: " + str(difference))
+print("Multiplication: " + str(multipl))
