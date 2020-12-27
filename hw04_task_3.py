8e0e7f4aaa10b78537eb0d826faceb28f778bcbd
Andrei Brazhelovich <baatol.mr@gmail.com>
2020-12-27 18:00:19 +0300

    3nd task is done

---
+++ hw04_task_3.py
@@ -0,0 +1,7 @@
+'''Создать строку равную первым пяти символам
+введенной строки.'''
+
+input_str = input('Please enter enything: ')
+new_str = input_str[:5]
+
+print('Your new string looks as follows: ' + str(new_str))
