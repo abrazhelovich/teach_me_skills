721f72c7c81f555358eb893a008f5c520fcc9c79
Andrei Brazhelovich <baatol.mr@gmail.com>
2020-12-27 17:47:20 +0300

    2nd task is done

---
+++ hw04_task_2.py
@@ -0,0 +1,7 @@
+'''Создать строку равную предпоследнему символу
+введенной строки.'''
+
+input_str = input('Please enter enytning: ')
+new_srt = input_str[-2]
+
+print('Your new string is: ' + str(new_srt))
