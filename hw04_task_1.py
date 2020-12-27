b515db31788ed0d2a6a9c47556adbb7e2296fe6a
Andrei Brazhelovich <baatol.mr@gmail.com>
2020-12-27 17:32:26 +0300

    1st task is done

---
+++ hw04_task_1.py
@@ -0,0 +1,7 @@
+'''Создать строку равную третьему символу введенной
+строки.'''
+
+input_str = input('Please enter anything: ')
+create_new_str_3_item = input_str[2]
+
+print('Your new string is 3rd character: ' + str(create_new_str_3_item))
