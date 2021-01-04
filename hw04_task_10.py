ef7f4a76566aa71863eca1a38382b5944fb3c2fa
Andrei Brazhelovich <baatol.mr@gmail.com>
2021-01-04 19:47:43 +0300

    1st task related with FOR

--- hw04_task_10.py
+++ hw04_task_10.py
@@ -5,8 +5,14 @@
 list_of_int = [1, 2, 3, 4, 5]
 new_list = []
 
-n = 0
-while n < len(list_of_int):
-    new_list.append(list_of_int[n] * -2)
-    n += 1
+#n = 0
+#while n < len(list_of_int):
+#    new_list.append(list_of_int[n] * -2)
+#    n += 1
+#print(new_list)
+
+#i = 0
+for i in list_of_int:
+    print(i)
+    new_list.append(i * -2)
 print(new_list)
