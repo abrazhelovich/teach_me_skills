947f5ce241c825f141d688d9abbb2821ae712402
Andrei Brazhelovich <baatol.mr@gmail.com>
2021-01-06 20:56:16 +0300

    4 task related with FOR

--- hw04_task_13.py
+++ hw04_task_13.py
@@ -1,15 +1,11 @@
 '''Дан список. Создать новый список, сдвинутый на 1
 элемент влево Пример:12345 -> 23451'''
 
-my_list = [1, 3, 4, 5, 6, 7, 8, 9, 'test']
+my_list = [3, 4, 5, 6, 7, 8, 9]
 new_list = []
 
-i = 1
-while i < len(my_list):
-    new_list.append(my_list[i])
-    i += 1
-
+for i in my_list[1:]:
+    new_list.append(i)
 new_list.append(my_list[0])
 
-print(my_list)
 print(new_list)
