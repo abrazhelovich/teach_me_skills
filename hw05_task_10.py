43e9ee71fe1a01503db2a96c570898d9eedd13cc
Andrei Brazhelovich <baatol.mr@gmail.com>
2021-01-13 17:00:58 +0300

    task 10: Schedule trains is done

--- hw05_task_10.py
+++ hw05_task_10.py
@@ -4,14 +4,41 @@
 # пребывания в пути которых превышает 7 часов 20
 # минут. (Примечание: использовать возможности
 # библиотеки datetime).
-import datetime
-from datetime import time
 
-lst = [{'train_code': '887B', 'station': 'Minsk', 'time_arrive': time(12, 00), 'time_departure': time(5, 40)},
-       {'train_code': '411C', 'station': 'Borisov', 'time_arrive': time(11, 15), 'time_departure': time(7, 10)},
-       {'train_code': '743L', 'station': 'Brest', 'time_arrive': time(8, 45), 'time_departure': time(1, 25)},
-       {'train_code': '113F', 'station': 'Orsha', 'time_arrive': time(13, 30), 'time_departure': time(11, 10)}]
+from datetime import datetime
+
+lst = [
+    {
+        'train_code': '88',
+        'from': 'Minsk',
+        'to': 'Brest',
+        'departure': datetime(2021, 1, 14, 10, 15),
+        'arrival': datetime(2021, 1, 14, 18, 44)
+    },
+    {
+        'train_code': '34',
+        'from': 'Minsk',
+        'to': 'Vilnus',
+        'departure': datetime(2021, 1, 15, 12, 24),
+        'arrival': datetime(2021, 1, 15, 14, 25)
+    },
+    {
+        'train_code': '95',
+        'from': 'Piter',
+        'to': 'Moscow',
+        'departure': datetime(2021, 1, 13, 12, 55),
+        'arrival': datetime(2021, 1, 15, 21, 17)
+    },
+    {
+        'train_code': '100',
+        'from': 'Moscow',
+        'to': 'Kiev',
+        'departure': datetime(2021, 1, 16, 10, 00),
+        'arrival': datetime(2021, 1, 18, 20, 23)
+    }]
 
 for i in lst:
-    print(i['time_departure'])
-    #print(datetime.time(i['time_arrive']))
+    delta = 7 * 3600 + 20 * 60
+    diff = (i['arrival'] - i['departure']).seconds
+    if diff > delta:
+         print(i)
