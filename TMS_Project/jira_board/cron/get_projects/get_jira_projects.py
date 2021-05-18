bda693b3e1ee152bbf2608395359ffb7cbfd2443
abrazhelovich <76255382+abrazhelovich@users.noreply.github.com>
2021-05-19 02:18:14 +0300

    Update get_jira_projects.py

--- TMS_Project/jira_board/cron/get_projects/get_jira_projects.py
+++ TMS_Project/jira_board/cron/get_projects/get_jira_projects.py
@@ -5,7 +5,7 @@ from add_rows_to_db import add_rows
 
 url = "https://pobe.atlassian.net/rest/api/3/project/"
 
-auth = HTTPBasicAuth("andreibandrei1506@gmail.com", "mTIFadgdYFPmbtf62beI1F1F")  #pobe project
+auth = HTTPBasicAuth()  
 
 headers = {
    "Accept": "application/json"
@@ -18,8 +18,6 @@ response = requests.request(
    auth=auth
 )
 
-#print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-
 decode = response.content.decode('UTF-8')
 result = json.loads(decode)
 
@@ -28,4 +26,4 @@ for i in result:
    key = i['key']
    name = i['name']
    add_rows(number, key, name)
-   #print(f'{key}  {name}')
+   
