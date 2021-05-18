3edef022569f9149f3df55c9580f84a43ed41907
abrazhelovich <76255382+abrazhelovich@users.noreply.github.com>
2021-05-19 02:16:19 +0300

    Update get_all_fields.py

--- TMS_Project/jira_board/cron/get_fields/get_all_fields.py
+++ TMS_Project/jira_board/cron/get_fields/get_all_fields.py
@@ -1,5 +1,3 @@
-# This code sample uses the 'requests' library:
-# http://docs.python-requests.org
 import requests
 from requests.auth import HTTPBasicAuth
 import json
@@ -7,10 +5,8 @@ from add_fields_to_db import add_rows
 
 url = "https://pobe.atlassian.net/rest/api/3/field"
 
-#url = "https://owp.atlassian.net/rest/api/3/field"
+auth = HTTPBasicAuth()  
 
-auth = HTTPBasicAuth("andreibandrei1506@gmail.com", "mTIFadgdYFPmbtf62beI1F1F")  #pobe project
-#auth = HTTPBasicAuth("andrei_brazhalovich@epam.com", "xp74GdrD1zuWdbJU6kpc2964")  # owp
 
 headers = {
    "Accept": "application/json"
@@ -23,15 +19,11 @@ response = requests.request(
    auth=auth
 )
 
-#print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-
 decode = response.content.decode('UTF-8')
 result = json.loads(decode)
 
 for i in result:
    key = i['key']
    name = i['name']
-   #if i['key'] != 'statuscategorychangedate' or i['key'] != 'issuetype':
-   #print(f'{key}  {name}')
    add_rows(key, name)
 
